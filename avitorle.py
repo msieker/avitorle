345c9f330f190f265d73375cbe5046ab5bef8271
Matt Sieker <matt@siekernet.com>
2013-02-17 01:41:55 -0600

    Get issues with writing out bytes fixed

--- avitorle.py
+++ avitorle.py
@@ -3,6 +3,7 @@
 import cv
 import itertools
 import math
+import array
 from struct import *
 
 filename = '/media/BadApple.mp4'
@@ -27,29 +28,40 @@ binframe = cv.CreateImage((newwidth,newheight),cv.IPL_DEPTH_8U,1)
 def processimagebuffer(imgbuffer, f):
     runs=[(len(list(group)),name) for name, group in itertools.groupby(imgbuffer)]
     output = []
-    smallruns = []    
+    smallruns = bytearray()
+
+#    for b in imgbuffer:
+#        f.write(pack('BB',1,b))
     
     for run in runs:
-        if run[0] > minrun:
-            
-            if len(smallruns)>0:
-                f.write(pack('cs',chr(len(smallruns)),''.join(smallruns)))
-                smallruns = []
+        if run[0] > minrun:            
+            if len(smallruns)>0:                
+#                print 'writing smallruns before run', len(smallruns), ':', smallruns
+                smallruns.insert(0,len(smallruns))
+                f.write(smallruns)
+                smallruns = bytearray()
             
             if run[0] > maxcopy:
+#                print 'runlength:', run[0]
                 runcount = int(math.floor(run[0]/maxcopy))
                 remainder = run[0] % maxcopy
                 if runcount > 0:
-                    f.write(''.join([pack('cc',chr(128+maxcopy),chr(run[1]))]*runcount))
+#                    print 'writing', runcount, 'runs', maxcopy, 'of',run[1]
+                    f.write(''.join([pack('Bc',128+maxcopy,chr(run[1]))]*runcount))
                 if remainder > 0:
-                    f.write(pack('cc',chr(128+remainder),chr(run[1])))
+#                    print 'writing run', remainder, 'of',run[1]
+                    f.write(pack('Bc',128+remainder,chr(run[1])))
             else:
-                f.write(pack('cc',chr(run[0]),chr(run[1])))
+#                print 'writing run', run[0], 'of',run[1]
+                f.write(pack('Bc',128+run[0],chr(run[1])))                
         else:
             if len(smallruns) + run[0] > maxcopy:
-                f.write(pack('cs',chr(len(smallruns)),''.join(smallruns)))
-                smallruns = []
-            smallruns.extend([chr(run[1])]*run[0])
+#                print 'writing smallruns', len(smallruns), ':', smallruns
+                smallruns.insert(0,len(smallruns))
+                f.write(smallruns)
+                smallruns = bytearray()
+
+            smallruns.extend([run[1]]*run[0])
     return output
 
 for i in xrange(0, int(cv.GetCaptureProperty(capture, cv.CV_CAP_PROP_FRAME_COUNT)),2):
@@ -61,11 +73,11 @@ for i in xrange(0, int(cv.GetCaptureProperty(capture, cv.CV_CAP_PROP_FRAME_COUNT
 
     mat =cv.GetMat(bwframe)
 
-    imagebuffer=[0]*((newheight-1)*(newwidth-1))
+    imagebuffer=[0]*((newheight)*(newwidth))
     offset=0
     print i
-    for y in range(0,newheight-1):
-        for x in range(0,newwidth-1):
+    for y in xrange(newheight):
+        for x in xrange(newwidth):
             imagebuffer[offset]=int(mat[y,x])
             offset+=1
     with open(outputpath + str(i).zfill(4) + '.rle','wb') as f:
